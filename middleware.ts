c83ca2ea378996961a1d9c7d38257f3c33595852
matthiasswartz-cell <matthiasswartz@gmail.com>
2026-02-19 14:13:49 -0500

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,23 +1,18 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
 const isProtectedRoute = createRouteMatcher([
-  "/dashboard(.*)",
-  "/api/customers(.*)",
-  "/api/inventory(.*)",
-  "/api/contracts(.*)",
-  "/api/payments(.*)",
-  "/api/iot(.*)",
+"/dashboard(.*)",
+"/api(.*)",
 ]);
 
-export default clerkMiddleware(async (auth, request) => {
-  if (isProtectedRoute(request)) {
-    await auth.protect();
-  }
+export default clerkMiddleware((auth, req) => {
+if (isProtectedRoute(req)) auth().protect();
 });
 
 export const config = {
-  matcher: [
-    "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
-    "/(api|trpc)(.*)",
-  ],
+matcher: [
+"/((?!.*\\..*|_next).*)",
+"/",
+"/(api|trpc)(.*)",
+],
 };
